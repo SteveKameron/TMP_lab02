1bb5a7912ac962029416582e8e4c948183558dd4
Steve Kameron <144481678+SteveKameron@users.noreply.github.com>
2024-05-02 17:54:36 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,11 +4,18 @@
 int
 main()
 {
+<<<<<<< HEAD
   stirng name;
 std:
   cout << "Enter name : " << std::endl;             // show text
   std::cin >> name;                                 // enter name
   std::cout << "hello world " << name << std::endl; // result
+=======
+stirng name;
+std:cout << "Enter name : " << std::endl; //show text.
+std::cin >> name; //enter name.
+std::cout << "hello world " << name << std::endl; //result.
+>>>>>>> c41b44f (Update hello_world.cpp)
 }
 
 EOF
